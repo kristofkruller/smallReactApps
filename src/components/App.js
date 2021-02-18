64c00688bb5b89f4214a48bbd8899c143e1292dc
Kristof Kruller <kristof.kruller@gmail.com>
2021-02-18 14:37:33 +0100

    building AuthContext

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import SignUp from './SignUp';
 
 function App() {
   return (
-  <Container className='d-flex align-items-center justify-content-center' style={{minHeight: '100vh'}}>
+  <Container className='d-flex align-items-center justify-content-center' 
+  style={{minHeight: '100vh'}}>
     <div className='w-100' style={{ maxWidth: '400px'}}>
     <SignUp />
     </div>
